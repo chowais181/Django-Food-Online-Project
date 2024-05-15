5e58b30ec295e09e545b72d0ce65c97f21857b1a
chowais181 <zahidawais98@gmail.com>
2024-05-15 21:04:21 +0500

    vendor, customer dashboards and sidebars

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
 
 urlpatterns =[ 
@@ -16,6 +16,9 @@ urlpatterns =[
     path('forgot_password/', views.forgot_password, name='forgot_password'),
     path('reset_password_validate/<uidb64>/<token>/', views.reset_password_validate, name='reset_password_validate'),
     path('reset_password/', views.reset_password, name='reset_password'),
+    
+     path('vendor/', include('vendor.urls')),
+   #  path('customer/', include('customers.urls')),
 
 
  ]
--- accounts/views.py
+++ accounts/views.py
@@ -10,6 +10,7 @@ from django.contrib.auth.decorators import login_required,user_passes_test
 from django.core.exceptions import PermissionDenied
 from .utils import detectUser,send_verification_email
 from django.template.defaultfilters import slugify
+from vendor.models import Vendor
 
 # Restrict the vendor from accessing the customer page
 def check_role_vendor(user):
@@ -169,7 +170,11 @@ def custDashboard(request):
 @login_required(login_url="login")
 @user_passes_test(check_role_vendor)
 def vendorDashboard(request):
-   return render(request,'accounts/vendorDashboard.html')
+   vendor = Vendor.objects.get(user=request.user)
+   context = {
+       'vendor': vendor,
+   }
+   return render(request,'accounts/vendorDashboard.html',context)
 
 
 def forgot_password(request):
